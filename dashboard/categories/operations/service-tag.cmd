b49867f3469cf142fdcadef271327150c6065bd8
Edmund Sutcliffe <github@panic.fluff.org>
2025-10-21 16:44:23 +0100

    fix reporting on entra-id

--- dashboard/categories/operations/service-tag.cmd
+++ dashboard/categories/operations/service-tag.cmd
@@ -1,1 +1,4 @@
-jq -r ".scenarios[0].steps[3].stdout" service-tag.log | jq -r ".[] | \"Service: \(.service)\", (if (.roles | length) > 0 then .roles[] | \"  - \(.)\" else \"  - (No roles defined)\" end)"
+@echo off
+rem provide a list of roles by service
+rem this comes from the gherkin implementation approach
+jq -r ".scenarios[0].steps[3].stdout" service-tag.stdout | jq -r ".[] | \"Service: \(.service)\", (if (.roles | length) > 0 then .roles[] | \"  - \(.)\" else \"  - (No roles defined)\" end)"
